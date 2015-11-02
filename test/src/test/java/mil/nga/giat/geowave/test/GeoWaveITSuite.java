b4f313337ed7c21e8ba9501cd5081f1fd3ef0560
Derek Yeager <dcy2003@gmail.com>
2015-11-02 08:46:11 -0500

    Adding GeoWaveRasterIT and KDERasterResizeIT back to GeoWaveITSuite

--- test/src/test/java/mil/nga/giat/geowave/test/GeoWaveITSuite.java
+++ test/src/test/java/mil/nga/giat/geowave/test/GeoWaveITSuite.java
@@ -1,15 +1,10 @@
 package mil.nga.giat.geowave.test;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
 import mil.nga.giat.geowave.test.mapreduce.BasicMapReduceIT;
 import mil.nga.giat.geowave.test.mapreduce.BulkIngestInputGenerationIT;
 import mil.nga.giat.geowave.test.mapreduce.DBScanIT;
 import mil.nga.giat.geowave.test.mapreduce.GeoWaveKMeansIT;
+import mil.nga.giat.geowave.test.mapreduce.KDERasterResizeIT;
 import mil.nga.giat.geowave.test.mapreduce.MapReduceTestEnvironment;
 import mil.nga.giat.geowave.test.query.AttributesSubsetQueryIT;
 import mil.nga.giat.geowave.test.query.SecondaryIndexingDriver;
@@ -17,19 +12,27 @@ import mil.nga.giat.geowave.test.query.SecondaryIndexingQueryTest;
 import mil.nga.giat.geowave.test.service.GeoServerIT;
 import mil.nga.giat.geowave.test.service.ServicesTestEnvironment;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
 @RunWith(Suite.class)
 @SuiteClasses({
-	//GeoWaveBasicIT.class, FIXME currently failing due to assertion failure and resource leakage
-	//GeoWaveRasterIT.class, FIXME currently failing due to two NPEs
-	// BasicKafkaIT.class, FIXME does not terminate properly due to incorrect kafka ingest & stage command arguments
+	// GeoWaveBasicIT.class, FIXME currently failing due to assertion failure
+	// and resource leakage
+	GeoWaveRasterIT.class,
+	// BasicKafkaIT.class, FIXME does not terminate properly due to incorrect
+	// kafka ingest & stage command arguments
 	BasicMapReduceIT.class,
 	BulkIngestInputGenerationIT.class,
-	//KDERasterResizeIT.class, FIXME currently failing due to NPE
+	KDERasterResizeIT.class,
 	GeoWaveKMeansIT.class,
-	//GeoWaveNNIT.class, FIXME currently failing due to assertion error
+	// GeoWaveNNIT.class, FIXME currently failing due to assertion error
 	GeoServerIT.class,
-	//GeoWaveServicesIT.class, FIXME does not terminate properly
-	//GeoWaveIngestGeoserverIT.class, FIXME currently failing
+	// GeoWaveServicesIT.class, FIXME does not terminate properly
+	// GeoWaveIngestGeoserverIT.class, FIXME currently failing
 	AttributesSubsetQueryIT.class,
 	SecondaryIndexingDriver.class,
 	SecondaryIndexingQueryTest.class,
