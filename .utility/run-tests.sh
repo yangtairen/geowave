46eb608ecc48b1ea64d39f1da672d8860eafcbdf
Rich Fecher <rfecher@gmail.com>
2017-05-16 11:19:39 -0400

    wip: tests for cdh

--- .utility/run-tests.sh
+++ .utility/run-tests.sh
@@ -3,7 +3,7 @@ set -ev
 
 if [ "$IT_ONLY" == "true" ]; then
   echo -e "Skipping unit tests w/ verify...\n"
-  mvn -q verify -Dtest=SkipUnitTests -DfailIfNoTests=false -P $MAVEN_PROFILES
+  mvn verify -Dtest=SkipUnitTests -DfailIfNoTests=false -P $MAVEN_PROFILES
 else
   echo -e "Running unit tests only w/ verify...\n"
   mvn -q verify -P $MAVEN_PROFILES
